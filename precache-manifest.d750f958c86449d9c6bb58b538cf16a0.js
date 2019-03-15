daa03aa10f01ba14c9e2f04c08577de01a2e794b
pkozhushko <petro.kozhushko17@gmail.com>
2019-03-15 17:04:21 +0200

    Updates

---
+++ precache-manifest.d750f958c86449d9c6bb58b538cf16a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66ad1057ef487c3b379f",
+    "url": "/articlesApp/static/js/runtime~main.4278b008.js"
+  },
+  {
+    "revision": "aac45580cb9d90d18302",
+    "url": "/articlesApp/static/js/main.c26808f3.chunk.js"
+  },
+  {
+    "revision": "6d122e3808195037b8db",
+    "url": "/articlesApp/static/js/2.c9807054.chunk.js"
+  },
+  {
+    "revision": "aac45580cb9d90d18302",
+    "url": "/articlesApp/static/css/main.e8c11d90.chunk.css"
+  },
+  {
+    "revision": "56b1f909e16020740aee533ac057b020",
+    "url": "/articlesApp/index.html"
+  }
+];
